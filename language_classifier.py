e991fe51f42fb74742b290a230e0d06681171b9d
Ali Mothana <aamothana@cps.edu>
2023-03-19 15:41:28 -0500

    Update

--- language_classifier.py
+++ language_classifier.py
@@ -1,8 +1,14 @@
+import collections
 import typing
 import math
-import pip
+
 import nltk
+nltk.download('udhr')
 from nltk.corpus import udhr
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
 languages = ['english', 'german', 'dutch', 'french', 'italian', 'spanish']
 language_ids = ['English-Latin1', 'German_Deutsch-Latin1', 'Dutch_Nederlands-Latin1', 'French_Francais-Latin1', 'Italian_Italiano-Latin1', 'Spanish_Espanol-Latin1']# I chose the above sample of languages as they all use similar characters. 
@@ -14,7 +20,6 @@ def retrieve_fileid_by_first_letter(fileids, letter):
     return [id for id in fileids if id.lower().startswith(letter.lower())]
 
 # Example usage
-print(f"Fileids beginning with 'R': {retrieve_fileid_by_first_letter(udhr.fileids(), letter='R')}")
 
 # Then copy-paste the language name and language id into the relevant list:
 languages += []
@@ -41,11 +46,9 @@ def build_model(text:str, n_vals: typing.List[int]) -> typing.Dict[str, int]:
     return model
 
 raw_texts = {language: udhr.raw(language_id) for language, language_id in zip(languages, language_ids)}
-print(raw_texts['english'][:1000]) # Just print the first 1000 characters
 
 # Build a model of each language
 models = {language: build_model(text=raw_texts[language], n_vals=range(1,4)) for language in languages}
-print(models['german'])
 
 def calculate_cosine(a: typing.Dict[str, float], b: typing.Dict[str, float]) -> float:
     """
@@ -79,21 +82,99 @@ def identify_language(
     for m in language_models:
         c = calculate_cosine(language_models[m], text_model)
         # The following line is just for demonstration, and can be deleted
-        print(f'Language: {m}; similarity with test text: {c}')
         if c > max_c:
             max_c = c
             language = m
     return language
 
-print(f"Test text: {text}")
-print(f"Identified language: {identify_language(text, models, n_vals=range(1,4))}")
+## ZackBOt
+zackBot = ChatBot("Zack")
+trainer = ListTrainer(zackBot)
+    
+## Greetingsgreet
+greetings = ["YO",
+            "*smacks you on your back*", 
+            "*shoots you*", 
+            "Hi",
+            "Oh lordy lord!",
+            "LMAO",
+            "dud",
+            "AUUUGHHHHHH",
+            "I got options bro",
+            "Yo can we finish this"
+            ]
+## Jokes
+jokes = ["Wanna hear a joke?"
+        "I was wondering why the ball was getting bigger and bigger. Then it hit me.",
+        "Why did the chicken cross the road?",
+        "Guess what? Chicken butt",
+        "What goes up and down but does not move?",
+        "Stairs",
+        "Where should a 500 pound alien go?",
+        "On a diet",
+        "What did one toilet say to the other?",
+        "You look a bit flushed.",
+        "Why did the picture go to jail?",
+        "Because it was framed.",
+        "What did one wall say to the other wall?",
+        "I'll meet you at the corner.",
+        "What did the paper say to the pencil?",
+        "Write on!",
+        "What do you call a boy named Lee that no one talks to?",
+        "Lonely"]
 
-# Prints
-# Test text: i was taught that the way of progress was neither swift nor easy.
-# Language: english; similarity with test text: 0.7812347488239613
-# Language: german; similarity with test text: 0.6638235631734796
-# Language: dutch; similarity with test text: 0.6495872103674768
-# Language: french; similarity with test text: 0.7073331083503462
-# Language: italian; similarity with test text: 0.6635204671187273
-# Language: spanish; similarity with test text: 0.6811923819801172
-# Identified language: english
+## Makeing fun of ali
+ali_insults = ["Why you stuttering", 
+            "I dont even play this and I can still beat you, you are bad!!!!!!",
+            "You're trash",
+            "Play chess rn",
+            "Can you give me a charger",
+            "Didn't you stutter?",
+            "man you so stupid",
+            "Why you stutter on that word?",
+            "Yo you are so trash bro"]
+    
+
+## Videogames
+videogames = ["Yesterday I was playing games last night and I went crazy!!!!!",
+              "Yo I am cracked at snake",
+             "1v1 me right now and I'll beat you",
+             "My KD is 5.0"]
+
+## Insults/Comebacks
+insults = ["man you so stupid",
+         "Why you stutter on that word?",
+         "Yo you are so trash bro",
+         ]
+
+## Training the ZackBot
+trainer.train(greetings)
+trainer.train(jokes)
+trainer.train(ali_insults)
+trainer.train(videogames)         
+trainer.train(insults)
+
+## Starting the conversation
+
+print("Hello! Welcome to ZackBot")
+print("To exit, please type exit")
+print()
+print("--------------------------------------------------")
+print()
+print("Hey")
+
+
+## Conversation
+while True:
+    response = input().lower()
+    if "exit" in response or "bye" in response:
+        break
+    elif identify_language(response, models, n_vals=range(1,4)) != 'english':
+        print("Sorry, I don't speak that language!")
+    else:
+        zackBot_response = zackBot.get_response(response)
+        print(zackBot_response)
+    user_response = response
+
+    
+print("ight ill see ya")
